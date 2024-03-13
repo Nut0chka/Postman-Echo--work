fd4b2d430878779dc1adc30a6d114297479e0493
Anna Demidova <annad.va00@gmail.com>
2024-03-14 00:01:08 +0300

    BUILD SUCCESSFUL

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,7 +14,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Bye"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
